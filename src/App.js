c201692da9abee94d33c5042b6c27ec2a19e007f
darwinmorales <darwin.morales.tech@gmail.com>
2016-10-03 17:10:31 +1100

    kanban board

--- src/App.js
+++ src/App.js
@@ -1,21 +1,43 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
 import './App.css';
 
+
+// Parent Component
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React Hello World</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
+
+    render() {
+        return (
+            <ul>
+                <ListItem quantity="1" type="wheat">Bread</ListItem>
+                <ListItem quantity="6" type="poultry">Eggs</ListItem>
+                <ListItem quantity="2" type="drink">Milk</ListItem>
+                {this.props.cards[1].title}
+                {this.props.cards[1].status}
+                <Cards card={this.props.cards[1]} />
+            </ul>
+        );
+    }
 }
 
+// Child Component
+class ListItem extends Component {
+    render() {
+        return (
+            <li>
+                {this.props.quantity}× {this.props.children} type: {this.props.type}
+            </li>
+        );
+    }
+}
+
+class Cards extends  Component {
+    render() {
+        return (
+            <div>
+                children: {this.props.children}
+                <p> status: {this.props.card.title} </p>
+            </div>
+        );
+    }
+}
 export default App;
---
+++ src/Card.js
@@ -0,0 +1,18 @@
+/**
+ * Created by darwinmorales on 3/10/2016.
+ */
+import React, {Component} from 'react';
+import CheckList from './CheckList';
+class Card extends Component {
+    render() {
+        return (
+            <div className="card">
+                <div className="card__title">{this.props.title}</div>
+                <div className="card__details">
+                    {this.props.description}
+                    <CheckList cardId={this.props.id} tasks={this.props.tasks}/>
+                </div>
+            </div> );
+    }
+}
+export default Card;
