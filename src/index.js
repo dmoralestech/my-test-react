9589d611bc82007ffb4e7c67d577f48ef4d9b45d
darwinmorales <darwin.morales.tech@gmail.com>
2016-10-04 21:12:09 +1100

    search text box.

--- src/index.js
+++ src/index.js
@@ -1,29 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import KanbanBoard from './KanbanBoard';
-import './index.css';
+import Search from './Search';
 
-let cardsList = [{
-    id: 1,
-    title: "Read the Book",
-    description: "I should read the book", color: '#BD8D31',
-    status: "in-progress",
-    tasks: []
-}, {
-    id: 2,
-    title: "Write some code",
-    description: "Code along with the samples ... at [github](https://github.com/pro-react)", color: '#3A7E28',
-    status: "todo",
-    tasks: [
-        {id: 1, name: "ContactList Example", done: true},
-        {id: 2, name: "Kanban Example", done: false},
-        {id: 3, name: "My own experiments", done: false}
-    ]
-},
-];
-// ReactDOM.render(
-//   <App cards={cardsList}/>,
-//   document.getElementById('root')
-// );
-
-ReactDOM.render(<KanbanBoard cards={cardsList}/>, document.getElementById('root'));
+ReactDOM.render(<Search/>, document.getElementById('root'));
---
+++ src/old_index.js
@@ -0,0 +1,36 @@
+/**
+ * Created by darwinmorales on 4/10/2016.
+ */
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+import KanbanBoard from './KanbanBoard';
+import Search from './Search';
+import './index.css';
+
+let cardsList = [{
+    id: 1,
+    title: "Read the Book",
+    description: "I should read the book", color: '#BD8D31',
+    status: "in-progress",
+    tasks: []
+}, {
+    id: 2,
+    title: "Write some code",
+    description: "Code along with the samples ... at [github](https://github.com/pro-react)", color: '#3A7E28',
+    status: "todo",
+    tasks: [
+        {id: 1, name: "ContactList Example", done: true},
+        {id: 2, name: "Kanban Example", done: false},
+        {id: 3, name: "My own experiments", done: false}
+    ]
+},
+];
+// ReactDOM.render(
+//   <App cards={cardsList}/>,
+//   document.getElementById('root')
+// );
+
+//ReactDOM.render(<KanbanBoard cards={cardsList}/>, document.getElementById('root'));
+ReactDOM.render(<Search/>, document.getElementById('root'));
+
